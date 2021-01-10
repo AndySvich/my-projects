0f60290925a6911094ac141a580363dd177a4842
Shivanand <sychikkalli@gmail.com>
2021-01-10 23:46:10 +0530

    merge sort

--- sorting/merge sort/main.c
+++ sorting/merge sort/main.c
@@ -3,7 +3,7 @@
 
 void merge(int A[], int low, int mid, int high)
 {
-	int size = high + 1;
+	int size = high - low + 1;
 	int* fArray = (int*)malloc(sizeof(int) * size);
 	int i = low , j = mid + 1 , k = 0;
 	while(k < size)
@@ -31,11 +31,32 @@ void merge(int A[], int low, int mid, int high)
 	{
 		A[a++] = fArray[m];
 	}
+
+	free(fArray);
+}
+
+void mergeSort(int A[], int size)
+{
+    int p;
+    for (p = 2; p <= size; p = p * 2)
+	{
+        for (int i = 0; i + p - 1 < size; i = i + p)
+		{
+            int low = i;
+            int high = i + p - 1;
+            int mid = (low + high) / 2;
+            merge(A, low, mid, high);
+        }
+    }
+    if (p / 2 < size)
+	{
+        merge(A, 0, p / 2 - 1, size-1);
+    }
 }
 
 int main(int argc, char** argv)
 {
-	int size = 10;
+	int size = 9;
 	int low = 0;
 	int mid = 3;
 	int* array = (int*)malloc(sizeof(int) * size);
@@ -54,7 +75,7 @@ int main(int argc, char** argv)
 		printf("%d ", array[i]);
 	}
 
-	merge(array, low, mid, size - 1);
+	mergeSort(array, size);
 
 	printf("\n ****** Merged Array ****** \n");
 	for(int i = 0 ; i < size ; i++)
